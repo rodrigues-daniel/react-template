f0e6301431e32cee37fc9c702c94416223f3ea10
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2018-10-28 22:18:04 -0300

    template base react

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+
+        return(
+        <div>
+            <h1>React App</h1>
+        </div>
+        );
+    }
+}
+
+export default App;
